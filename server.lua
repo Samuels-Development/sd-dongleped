357a4eb3f84aa7209a8de4ef47551509d72a06d9
CuPidev <m41hmc@gmail.com>
2022-05-22 17:30:32 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -60,47 +60,5 @@ RegisterServerEvent(
         else
             TriggerClientEvent("QBCore:Notify", source, "You don't have " .. item_price .. " cryptos", "error")
         end
-
-        -- if itemname == "electronickit" then
-        --     if Crypto >= 100 then
-        --         xPlayer.Functions.RemoveMoney("crypto", 100)
-        --         xPlayer.Functions.AddItem(itemname, 1)
-        --         TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items[itemname], "add")
-        --     else
-        --         TriggerClientEvent("QBCore:Notify", source, "You don't have 100 cryptos", "error")
-        --     end
-        -- elseif itemname == "gatecrack" then
-        --     if Crypto >= 250 then
-        --         xPlayer.Functions.RemoveMoney("crypto", 250)
-        --         xPlayer.Functions.AddItem(itemname, 1)
-        --         TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items[itemname], "add")
-        --     else
-        --         TriggerClientEvent("QBCore:Notify", source, "You don't have 250 cryptos", "error")
-        --     end
-        -- elseif itemname == "thermite" then
-        --     if Crypto >= 500 then
-        --         xPlayer.Functions.RemoveMoney("crypto", 500)
-        --         xPlayer.Functions.AddItem(itemname, 1)
-        --         TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items[itemname], "add")
-        --     else
-        --         TriggerClientEvent("QBCore:Notify", source, "You don't have 500 cryptos", "error")
-        --     end
-        -- elseif itemname == "trojan_usb" then
-        --     if Crypto >= 500 then
-        --         xPlayer.Functions.RemoveMoney("crypto", 500)
-        --         xPlayer.Functions.AddItem(itemname, 1)
-        --         TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items[itemname], "add")
-        --     else
-        --         TriggerClientEvent("QBCore:Notify", source, "You don't have 1500 cryptos", "error")
-        --     end
-        -- elseif itemname == "drill" then
-        --     if Crypto >= 100 then
-        --         xPlayer.Functions.RemoveMoney("crypto", 100)
-        --         xPlayer.Functions.AddItem(itemname, 1)
-        --         TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items[itemname], "add")
-        --     else
-        --         TriggerClientEvent("QBCore:Notify", source, "You don't have 100 cryptos", "error")
-        --     end
-        -- end
     end
 )
