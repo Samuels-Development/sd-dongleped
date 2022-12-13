920ee21598024ff558e5c80f6a61739792413fe5
Samuel Nicol <99494967+Github-Samuel@users.noreply.github.com>
2022-12-14 00:09:23 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -14,3 +14,14 @@ RegisterNetEvent('sd-dongle:server:buyshit', function(ped)
         TriggerClientEvent('QBCore:Notify', src, 'Not enough '..Config.Shop[ped].type, 'error')
     end
 end)
+
+QBCore.Functions.CreateCallback('sd-dongleped:server:getCops', function(source, cb)
+    local amount = 0
+    local players = QBCore.Functions.GetQBPlayers()
+    for _, Player in pairs(players) do
+        if Player.PlayerData.job.name == "police" and Player.PlayerData.job.onduty then
+            amount = amount + 1
+        end
+    end
+    cb(amount)
+end)
